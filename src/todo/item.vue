ce5eea6f3ba5331c68a6f0e89f6380e2d31973fb
jaris <jarisma@foxmail.com>
2019-03-22 18:57:52 +0800

    添加item的模板

---
+++ src/todo/item.vue
@@ -0,0 +1,36 @@
+<template>
+    <div :class="['todo-item',todo.completed?'completed':'']">
+        <input 
+            type="checkbox"
+            class="toggle"
+            v-model="todo.completed"
+        >
+        <label>{{todo.content}}</label>
+        <button class="destory" @click="deleteTodo"></button>
+    </div>
+</template>
+
+<script>
+export default {
+    props:{
+        todo:{
+            type:Object,
+            required:true
+        }
+    },
+    methods:{
+        deleteTodo(){
+
+        }
+    }
+}
+</script>
+
+<style lang="less">
+    .todo-item{
+        position: relative;
+        background-color: #fff;
+        
+    }
+</style>
+
