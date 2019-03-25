38c98e5dba1b186044cafcafef313f5978d713bd
jaris <jarisma@foxmail.com>
2019-03-25 17:53:21 +0800

    添加插件extract-text-webpack-plugin,配置生产环境

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const VueLoaderPlugin = require('vue-loader/lib/plugin');
 const webpack = require('webpack');
 const HTMLPlugin = require('html-webpack-plugin');
+const ExtractPlugin = require('extract-text-webpack-plugin'); //把非javascript的静态文件单独打包
 
 const isDev = process.env.NODE_ENV === 'development';
 
@@ -9,7 +10,7 @@ const config = {
     target: 'web',
     entry: path.join(__dirname, 'src/index.js'),
     output: {
-        filename: 'bundle.js',
+        filename: 'bundle.[hash:8].js', //开发环境中可以这么写
         path: path.join(__dirname, 'dist')
     },
     module: {
@@ -24,19 +25,6 @@ const config = {
             use: [
                 'style-loader',
                 'css-loader'
-            ]
-        }, {
-            test: /\.less$/,
-            use: [
-                'style-loader',
-                'css-loader',
-                {
-                    loader: 'postcss-loader',
-                    options: {
-                        sourceMap: true //编译更快
-                    }
-                },
-                'less-loader'
             ]
         }, {
             test: /\.(gif|svg|png|jpg|jpeg)$/,
@@ -61,6 +49,20 @@ const config = {
 }
 
 if (isDev) {
+    config.module.rules.push({
+        test: /\.less$/,
+        use: [
+            'style-loader', //将css写入javascript中
+            'css-loader',
+            {
+                loader: 'postcss-loader',
+                options: {
+                    sourceMap: true //编译更快
+                }
+            },
+            'less-loader'
+        ]
+    })
     config.devTool = '#cheap-module-eval-source-map', //映射代码，方便调试
         config.devServer = {
             port: 8080,
@@ -75,6 +77,27 @@ if (isDev) {
     config.plugins.push(
         new webpack.HotModuleReplacementPlugin(), //热加载
         new webpack.NoEmitOnErrorsPlugin()
+    )
+} else {
+    config.output.filename = '[name].[chunkhash:8].js'; //正式环境中要用chunkhash编码
+    config.module.rules.push({
+        test: /\.less$/,
+        use: ExtractPlugin.extract({
+            fallback: 'style-loader',
+            use: [
+                'css-loader',
+                {
+                    loader: 'postcss-loader',
+                    options: {
+                        sourceMap: true //编译更快
+                    }
+                },
+                'less-loader'
+            ]
+        })
+    });
+    config.plugins.push(
+        new ExtractPlugin('styles.[chunkhash:8].css')
     )
 }
 
